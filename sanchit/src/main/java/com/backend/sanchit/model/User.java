63c0910281cddb16dffb75b0fa9a0db37d5b0f59
garlicGrape <sanchit809@gmail.com>
2023-01-05 23:51:04 -0500

    Added annotations and table columns for user login

--- sanchit/src/main/java/com/backend/sanchit/model/User.java
+++ sanchit/src/main/java/com/backend/sanchit/model/User.java
@@ -1,6 +1,30 @@
 package com.backend.sanchit.model;
 
+import jakarta.persistence.*;
+import lombok.*;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+
+
 
 }
