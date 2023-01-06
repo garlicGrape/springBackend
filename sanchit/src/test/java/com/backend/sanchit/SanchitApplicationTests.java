ca60f8ff4ed27b93182f8cf0ba2ba6dc97082b18
garlicGrape <sanchit809@gmail.com>
2023-01-05 23:14:41 -0500

    Adding spring boot project to the repository

---
+++ sanchit/src/test/java/com/backend/sanchit/SanchitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sanchit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanchitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
