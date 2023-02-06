192e51d083ad74864a54b784b14f9aa2b226589a
garlicGrape <sanchit809@gmail.com>
2023-02-05 21:08:20 -0500

    Finished adding role code to users model.

--- sanchit/src/main/java/com/backend/sanchit/controller/UserController.java
+++ sanchit/src/main/java/com/backend/sanchit/controller/UserController.java
@@ -47,6 +47,7 @@ public class UserController {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not exist with id :" + id));
 
+        user.setEmail(userDetails.getEmail());
         user.setUsername(userDetails.getUsername());
         user.setPassword(userDetails.getPassword());
 
--- sanchit/src/main/resources/rolesScript.sql
+++ sanchit/src/main/resources/rolesScript.sql
@@ -1,3 +1,17 @@
 INSERT INTO roles(name) values('ROLE_USER')
 INSERT INTO roles(name) values('ROLE_MODERATOR')
 INSERT INTO roles(name) values('ROLE_ADMIN')
+
+CREATE TABLE users(
+	id serial PRIMARY KEY,
+	email varchar(255),
+	userpassword varchar(255),
+	username varchar(255)
+);
+
+INSERT INTO users(email,userpassword,username) VALUES('test@test.com','dog','Zelda');
+INSERT INTO users(email,userpassword,username) VALUES('test2@test.com','aligator','lizard');
+
+DROP TABLE users CASCADE;
+
+SELECT * FROM users; 
